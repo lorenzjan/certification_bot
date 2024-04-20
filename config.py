78889c7cd41f44655a0e069cb88c1680eef8c6b9
Jan Lorenz <77906722+lorenzjan@users.noreply.github.com>
2024-04-20 19:08:22 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Discord Bot-Token
-TOKEN = 'MTIyOTkwMTYzMzI2MjE5MDc2NA.GpNikx.0vi029rrrz_25c-dUin00H646hTCsPm5ZkA03s'
+TOKEN = ''
 
 # Discord Webhook-URL
-WEBHOOK_URL = 'https://discord.com/api/webhooks/1231288265139683399/BWu0i8XgzK6FpdoHLGbEASBH-3VyYqru89cb4xWDdYZmP4V3N_Lh9iBygCyb69m666c5'
+WEBHOOK_URL = ''
 
 # Filename for request counter
 COUNTER_FILE = "desktop/request_counter.txt"
